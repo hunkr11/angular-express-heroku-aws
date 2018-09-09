4394d7d872385dab8a7a11aa5db8fe98e91d3681
hunkr11 <hunkr11@gmail.com>
2018-09-09 18:56:20 +0530

    modified server dist

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
 // app.use(express.static(path.join(__dirname, './dist/hanonsystem-ui-new')));
-app.use(express.static('./dist/hanonsystem-ui-new'));
+app.use(express.static(__dirname+'./dist/hanonsystem-ui-new'));
 
 // API location
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/hanonsystem-ui-new/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/hanonsystem-ui-new/index.html'));
 });
 
 var corsOptions = {
